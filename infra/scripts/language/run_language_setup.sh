f75a4c54195ca56c6ff63421345710608419714d
Annabel Ng <annabelng@berkeley.edu>
2025-06-03 10:15:26 -0700

    fix agent id assignment

--- infra/scripts/language/run_language_setup.sh
+++ infra/scripts/language/run_language_setup.sh
@@ -22,7 +22,7 @@ python3 cqa_setup.py
 echo "Running Orchestration setup..."
 python3 orchestration_setup.py
 echo "Running agent setup..."
-TRIAGE_AGENT_id = $(python3 agent_setup.py | tail -n1)
+TRIAGE_AGENT_id=$(python3 agent_setup.py | tail -n1)
 export TRIAGE_AGENT_id
 # python3 agent_setup.py
 
