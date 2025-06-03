93e042dd23f53b055d62a773743124fd2557fc2f
Annabel Ng <annabelng@berkeley.edu>
2025-06-03 10:39:22 -0700

    update env variable name

--- infra/scripts/language/run_language_setup.sh
+++ infra/scripts/language/run_language_setup.sh
@@ -22,8 +22,9 @@ python3 cqa_setup.py
 echo "Running Orchestration setup..."
 python3 orchestration_setup.py
 echo "Running agent setup..."
-TRIAGE_AGENT_id=$(python3 agent_setup.py | tail -n1)
-export TRIAGE_AGENT_id
+TRIAGE_AGENT_ID=$(python3 agent_setup.py | tee /dev/tty | tail -n1)
+echo "Captured TRIAGE_AGENT_id: $TRIAGE_AGENT_ID"
+export TRIAGE_AGENT_ID
 # python3 agent_setup.py
 
 # Cleanup:
